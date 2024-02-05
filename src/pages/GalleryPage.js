d6b5a398e9a99dfe446fe4c0013777417aa24851
koke <rekoke92@gmail.com>
2024-02-05 19:47:01 +0100

    Added text and transitions on gallery

--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -1,12 +1,22 @@
+import data from "../assets/texts/text.json";
+
 const GalleryPage = () => {
-  const images = require.context("../assets", true);
+  const images = require.context("../assets/images", true);
   const imageList = images.keys().map((image) => images(image));
+  console.log("data---", data.texts);
 
   const ImageGrid = imageList.map((image, index) => (
-    <img key={index} src={image} alt={`img-${index}`} />
+    <li>
+      <span>{data.texts[index].text}</span>
+      <img key={index} src={image} alt={`img-${index}`} />
+    </li>
   ));
-  console.log("imageList", imageList);
-  return <div>{ImageGrid}</div>;
+
+  return (
+    <div className="Gallery">
+      <ul>{ImageGrid}</ul>
+    </div>
+  );
 };
 
 export default GalleryPage;
